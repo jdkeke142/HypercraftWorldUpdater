3199fd49570da0451dcf759481b3ed53bdebc4ac
kdkeke142 <jdkeke142@protonmail.com>
2019-04-29 23:34:52 +0200

    Improved conversion task by kicking all players, unload all loaded chunks, shutdown the server at the end of the conversion task

--- src/main/java/fr/keke142/worldupdater/ConvertMyWorldCommand.java
+++ src/main/java/fr/keke142/worldupdater/ConvertMyWorldCommand.java
@@ -12,10 +12,12 @@ import fr.keke142.worldupdater.providers.WorldGuardRegionProvider;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Chunk;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.plugin.PluginManager;
 
 import java.io.File;
@@ -54,109 +56,127 @@ public class ConvertMyWorldCommand implements CommandExecutor {
             return false;
         }
 
-        RegionProvider regionProvider;
+        Set<RegionProvider> regionProviders = new HashSet<>();
 
         PluginManager pluginManager = plugin.getPluginManager();
 
+        ConsoleCommandSender consoleSender = plugin.getServer().getConsoleSender();
+
         if (pluginManager.isPluginEnabled("WorldGuard")) {
-            regionProvider = new WorldGuardRegionProvider(WorldGuard.getInstance());
-            sender.sendMessage(ChatColor.GREEN + "Using WorldGuard as region provider.");
-        } else if (pluginManager.isPluginEnabled("GriefPrevention")) {
-            regionProvider = new GriefPreventionRegionProvider(GriefPrevention.instance);
-            sender.sendMessage(ChatColor.GREEN + "Using GriefPrevention as region provider.");
-        } else {
+            regionProviders.add(new WorldGuardRegionProvider(WorldGuard.getInstance()));
+            consoleSender.sendMessage(ChatColor.GREEN + "Added WorldGuard as region provider.");
+        }
+
+        if (pluginManager.isPluginEnabled("GriefPrevention")) {
+            regionProviders.add(new GriefPreventionRegionProvider(GriefPrevention.instance));
+            consoleSender.sendMessage(ChatColor.GREEN + "Added GriefPrevention as region provider.");
+        }
+
+        if (regionProviders.isEmpty()) {
             sender.sendMessage(ChatColor.RED + "Unable to find any region provider !");
             return false;
         }
 
-        try {
-            Set<UpdaterRegion> regions = regionProvider.getWorldRegions(world);
+        Set<UpdaterRegion> regions = new HashSet<>();
 
-            regions.forEach(region -> {
-
-                BlockVector3 min = region.getMinimumPoint().withY(WORLD_MIN_Y);
-                BlockVector3 max = region.getMaximumPoint().withY(WORLD_MAX_Y);
-
-                for (int x = min.getBlockX() >> CHUNK_SHIFTS; x <= max.getBlockX() >> CHUNK_SHIFTS; ++x) {
-                    for (int z = min.getBlockZ() >> CHUNK_SHIFTS; z <= max.getBlockZ() >> CHUNK_SHIFTS; ++z) {
-                        blacklistedChunks.add(BlockVector2.at(x, z));
-                    }
-                }
-
-                blacklistedChunks.forEach(chunk -> {
-                    int chunkX = chunk.getX();
-                    int chunkZ = chunk.getZ();
-
-                    int regionX = chunkX >> REGION_SHIFTS;
-                    int regionZ = chunkZ >> REGION_SHIFTS;
-
-                    blacklistedRegions.add(BlockVector2.at(regionX, regionZ));
-                });
-            });
-
-            File regionRoot = new File(worldName + File.separator + "region");
-            MCAQueue queue = new MCAQueue(worldName, regionRoot, true);
-
-            final MCAFile[] lastRegionFile = new MCAFile[1];
-
-            sender.sendMessage(ChatColor.YELLOW + "Deleting all chunks that are not touch region using FastAsyncWorldEdit FaweQueue...");
-
-            queue.filterWorld(new MCAFilter() {
-                @Override
-                public MCAFile applyFile(MCAFile mca) {
-                    int mcaX = mca.getX();
-                    int mcaZ = mca.getZ();
-
-                    BlockVector2 mcaVector = BlockVector2.at(mcaX, mcaZ);
-
-                    for (BlockVector2 region : blacklistedRegions) {
-                        if (mcaVector.equals(region)) {
-                            lastRegionFile[0] = mca;
-                            return mca;
-                        }
-                    }
-
-                    mca.close(ForkJoinPool.commonPool());
-                    mca.getFile().delete();
-
-                    sender.sendMessage(ChatColor.YELLOW + "Deleted region (" + mcaX + ";" + mcaZ + ")");
-                    return null;
-                }
-
-                @Override
-                public boolean appliesChunk(int cx, int cz) {
-                    if (!blacklistedChunks.contains(BlockVector2.at(cx, cz))) {
-                        try {
-                            MCAFile mcaFile = lastRegionFile[0];
-
-                            RandomAccessFile unChunk = new RandomAccessFile(mcaFile.getFile(), "rwd");
-
-                            int offsetX = mcaFile.getX() << REGION_SHIFTS;
-                            int offsetZ = mcaFile.getZ() << REGION_SHIFTS;
-
-                            long wipePos = 4 * ((cx - offsetX) + ((cz - offsetZ) * 32));
-                            unChunk.seek(wipePos);
-                            unChunk.writeInt(0);
-
-                            unChunk.close();
-                            sender.sendMessage(ChatColor.YELLOW + "Deleted chunk (" + cx + ";" + cz + ")");
-                        } catch (IOException e) {
-                            plugin.getLogger().severe(ChatColor.RED + "Error deleting chunk (" + cx + ";" + cz + ")");
-                            e.printStackTrace();
-                        }
-                    }
-                    return false;
-                }
-
-            });
-
-            sender.sendMessage(ChatColor.GREEN + "Conversion task finished !");
-
-            return true;
-        } catch (ResolvingRegionsException e) {
-            sender.sendMessage(ChatColor.RED + "Error during resolving world regions: " + e.getCause());
+        sender.sendMessage(ChatColor.YELLOW + "Merging providers together...");
+        for (RegionProvider provider : regionProviders) {
+            try {
+                regions.addAll(provider.getWorldRegions(world));
+            } catch (ResolvingRegionsException e) {
+                sender.sendMessage(ChatColor.RED + "Error during resolving world regions: " + e.getCause());
+                e.printStackTrace();
+            }
         }
 
-        return false;
+        regions.forEach(region -> {
+            BlockVector3 min = region.getMinimumPoint().withY(WORLD_MIN_Y);
+            BlockVector3 max = region.getMaximumPoint().withY(WORLD_MAX_Y);
+
+            for (int x = min.getBlockX() >> CHUNK_SHIFTS; x <= max.getBlockX() >> CHUNK_SHIFTS; ++x) {
+                for (int z = min.getBlockZ() >> CHUNK_SHIFTS; z <= max.getBlockZ() >> CHUNK_SHIFTS; ++z) {
+                    blacklistedChunks.add(BlockVector2.at(x, z));
+                }
+            }
+
+            blacklistedChunks.forEach(chunk -> {
+                int chunkX = chunk.getX();
+                int chunkZ = chunk.getZ();
+
+                int regionX = chunkX >> REGION_SHIFTS;
+                int regionZ = chunkZ >> REGION_SHIFTS;
+
+                blacklistedRegions.add(BlockVector2.at(regionX, regionZ));
+            });
+        });
+
+        File regionRoot = new File(worldName + File.separator + "region");
+        MCAQueue queue = new MCAQueue(worldName, regionRoot, true);
+
+        final MCAFile[] lastRegionFile = new MCAFile[1];
+
+        consoleSender.sendMessage(ChatColor.YELLOW + "Deleting all chunks that are not touch region using FastAsyncWorldEdit FaweQueue...");
+
+        Bukkit.getOnlinePlayers().forEach(player -> player.kickPlayer(ChatColor.YELLOW + "Starting world conversion, please check your console !"));
+
+        consoleSender.sendMessage(ChatColor.YELLOW + "Unloading all chunks of the world...");
+
+        for (Chunk loadedChunk : world.getLoadedChunks()) {
+            loadedChunk.unload();
+        }
+
+        queue.filterWorld(new MCAFilter() {
+            @Override
+            public MCAFile applyFile(MCAFile mca) {
+                int mcaX = mca.getX();
+                int mcaZ = mca.getZ();
+
+                BlockVector2 mcaVector = BlockVector2.at(mcaX, mcaZ);
+
+                for (BlockVector2 region : blacklistedRegions) {
+                    if (mcaVector.equals(region)) {
+                        lastRegionFile[0] = mca;
+                        return mca;
+                    }
+                }
+
+                mca.close(ForkJoinPool.commonPool());
+                mca.getFile().delete();
+
+                consoleSender.sendMessage(ChatColor.YELLOW + "Deleted region (" + mcaX + ";" + mcaZ + ")");
+                return null;
+            }
+
+            @Override
+            public boolean appliesChunk(int cx, int cz) {
+                if (!blacklistedChunks.contains(BlockVector2.at(cx, cz))) {
+                    try {
+                        MCAFile mcaFile = lastRegionFile[0];
+
+                        RandomAccessFile unChunk = new RandomAccessFile(mcaFile.getFile(), "rwd");
+
+                        int offsetX = mcaFile.getX() << REGION_SHIFTS;
+                        int offsetZ = mcaFile.getZ() << REGION_SHIFTS;
+
+                        long wipePos = 4 * ((cx - offsetX) + ((cz - offsetZ) * 32));
+                        unChunk.seek(wipePos);
+                        unChunk.writeInt(0);
+
+                        unChunk.close();
+                        consoleSender.sendMessage(ChatColor.YELLOW + "Deleted chunk (" + cx + ";" + cz + ")");
+                    } catch (IOException e) {
+                        plugin.getLogger().severe(ChatColor.RED + "Error deleting chunk (" + cx + ";" + cz + ")");
+                        e.printStackTrace();
+                    }
+                }
+                return false;
+            }
+
+        });
+
+        consoleSender.sendMessage(ChatColor.GREEN + "Conversion task finished ! Stopping the server to apply changes...");
+        Bukkit.shutdown();
+
+        return true;
     }
 }
